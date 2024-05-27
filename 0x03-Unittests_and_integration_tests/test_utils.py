efcd0a4066875b0eb23b07d35cd30092a32b26f6
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-05-27 18:09:33 +0100

    Make every .py file executable

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -15,3 +15,13 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """test access nested map method"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",), "KeyError: 'a'"),
+        ({"a": 1}, ("a", "b"), "KeyError: 'b"),
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Raises an exception"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")

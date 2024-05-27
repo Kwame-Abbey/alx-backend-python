efc615279a543b24419217283464b2e91bc09270
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-05-27 19:20:20 +0100

    Parameterize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -43,3 +43,28 @@ class TestGetJson(unittest.TestCase):
             result = get_json(test_url)
             mocked_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Test the memoize decorator"""
+
+    def test_memoize(self):
+        """Test that a_property calls a_method only once"""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mocked_m:
+            instance = TestClass()
+
+            result1 = instance.a_property
+            result2 = instance.a_property
+
+            mocked_m.assert_called_once()
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)

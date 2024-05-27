18cdc88180405b0c313cc9894869002ba168c4ad
Kwame-Abbey <constancekwameabbey@gmail.com>
2024-05-27 19:45:30 +0100

    Patch

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,27 +2,40 @@
 """Test module for GithubOrgClient"""
 import unittest
 from unittest.mock import patch, PropertyMock
-from parameterized import parameterized
+# from parameterized import parameterized
 from client import GithubOrgClient
 
 
 class TestGithubOrgClient(unittest.TestCase):
     """Test case for GithubOrgClient"""
 
-    @parameterized.expand([
-        ("google", {"login": "mock"}),
-        ("abc", {"login": "mock"}),
-    ])
-    @patch('client.get_json', return_value={"login": "mock"})
-    def test_org(self, org_name, expected_result, mock_get_json):
-        """Test that GithubOrgClient.org returns the correct value"""
-        client = GithubOrgClient(org_name)
-        result = client.org
+    @patch('client.get_json')
+    @patch('client.GithubOrgClient._public_repos_url',
+           new_callable=unittest.mock.PropertyMock)
+    def test_public_repos(self, mock_repos_url, mock_get_json):
+        """Test public_repos method"""
 
+        # Define mocked data
+        mock_repos_url.return_value = (
+            "https://api.github.com/orgs/test-org/"
+            "repos"
+        )
+
+        mock_get_json.return_value = [
+            {"name": "repo1", "license": {"key": "mit"}},
+            {"name": "repo2", "license": {"key": "apache"}},
+            {"name": "repo3", "license": {"key": "mit"}},
+        ]
+
+        client = GithubOrgClient("test-org")
+
+        repos = client.public_repos(license="mit")
+
+        mock_repos_url.assert_called_once()
         mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}")
+            "https://api.github.com/orgs/test-org/repos")
 
-        self.assertEqual(result, expected_result)
+        self.assertEqual(repos, ["repo1", "repo3"])
 
     def test_public_repos_url(self):
         """Test that _public_repos_url returns the correct
